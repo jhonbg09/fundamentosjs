970c3b8eca6f7651c4eec1458758212dd6e210cf
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-05 16:13:32 -0500

    Unir dos objetos o mas

---
+++ js/07.js
@@ -0,0 +1,26 @@
+// Unir dos objetos o mas
+
+const producto = {
+    nombre: "Tablet",
+    precio: 300,
+    disponible: true
+}
+
+
+const cliente = {
+    nombre: "Jhon",
+    premiun: true
+}
+
+
+// Hay que tener en cuenta que e objeto no se puede modificar 
+// Metodo 1
+
+// const nuevoObjeto = Object.assign(producto, cliente) No funciona este metodo por que se pierden datos 
+// console.table(nuevoObjeto)
+
+const nuevoCliente2 = {
+    producto:{...producto}, 
+    cliente:{...cliente}
+}
+console.table(nuevoCliente2)
