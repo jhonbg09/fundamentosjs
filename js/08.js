c42ec16b32ec3d645e274709803229a1f42cec8d
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-05 16:30:45 -0500

    Arrays

---
+++ js/08.js
@@ -0,0 +1,8 @@
+//Arrays 
+
+const tecnologias = [30, 29, 10, true, "React js", "CSS"]
+console.table(tecnologias)
+console.log(tecnologias[2])
+console.log(tecnologias[5])
+console.log(tecnologias.length)
+console.log(tecnologias.toString())
