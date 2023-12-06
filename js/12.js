05359a0eeefa59c9a4d89ca0f2cd529aec44bd1a
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-05 21:12:16 -0500

    Funciones - Function Expressions

--- js/12.js
+++ js/12.js
@@ -8,5 +8,11 @@ function sumaDos(a, b) {
     console.log( a + b);
 }
 
+function sumaTres(a = 0, b = 0) {
+    return a + b; //siempre que usamos esl return dentro de la funcion debemos guardar los parametros dentro de una variable 
+}
+
 sumar();
 sumaDos(5, 5)
+const resultado = sumaTres(100, 200); //asi se debe realizar con el return
+console.log(resultado);
---
+++ js/13.js
@@ -0,0 +1,10 @@
+//  Funciones - Function Expressions
+
+
+const sumaTres = function(a = 0, b = 0) {
+   return a + b;
+}
+
+const result = sumaTres(5, 5);
+
+console.log(result);
