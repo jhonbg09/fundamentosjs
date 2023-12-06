a80ef03046d7839c7764dc88a70175f2650485d1
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-06 14:44:43 -0500

    Condicionales condicion || y &&

---
+++ js/19.js
@@ -0,0 +1,11 @@
+//Consicionales condicion || y &&
+
+const saldo = 600
+const pagar = 500
+const tarjeta = false
+
+if(saldo > pagar || tarjeta){
+    console.log("Puedes pagar")
+} else {
+    console.log("No, no Puedes Pagar")
+}
